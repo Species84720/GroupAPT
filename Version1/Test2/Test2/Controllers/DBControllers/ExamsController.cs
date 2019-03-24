e4a37799c1eba9444f3462987f307994632a4805
species84720 <Species84720@users.noreply.github.com>
2019-03-25 00:14:47 +0100

    Examination Upgrades
    The exam is done through a post and different students get different answers. Plus some few debugger issues fixed and less data tranferred to client side.
    
    Co-Authored-By: markmifsud <markmifsud@users.noreply.github.com>

--- Version1/Test2/Test2/Controllers/DBControllers/ExamsController.cs
+++ Version1/Test2/Test2/Controllers/DBControllers/ExamsController.cs
@@ -9,6 +9,7 @@ using System.Web;
 using System.Web.Mvc;
 using Test2.Models;
 using Test2.Models.DBModels;
+using WebGrease.Css.Ast.Selectors;
 
 namespace Test2.Controllers.DBControllers
 {
@@ -17,59 +18,132 @@ namespace Test2.Controllers.DBControllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Exam
-        public async Task<ActionResult> Index(string id)
+        public async Task<ActionResult> Index()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ExamSession examSession = await db.ExamSessions.FindAsync(id);
-            if (examSession == null)
-            {
-                return HttpNotFound();
-            }
-
-            // we get the paperquestions for this exam in a list
-            List<PaperQuestion> paper = db.PaperQuestions.Where(x => x.ExamId == id).ToList();
-
-            //now we generate a list of QuestionId found in the list above
-            IEnumerable<int> SpecificQuestions = from p in paper select p.QuestionId;
-
-            //and we select those Questions that match the QuestionIds
-            List<Question> questionList = new List<Question>(from q in db.Questions where SpecificQuestions.Contains(q.QuestionId) select q);
-
-            //now we get the multiple choices we need for this exam
-            List<MultipleChoice> multichoiceList = new List<MultipleChoice>(from m in db.MultipleChoices where SpecificQuestions.Contains(m.QuestionId) select m);
-
-            ViewBag.MultiChoices = multichoiceList;
-            ViewBag.Questions = questionList;
-            ViewBag.PaperQuestions = paper;
-
-            //ViewBag.PaperQuestions = db.PaperQuestions.Where(x => x.ExamId == id).ToList();
-            //ViewBag.Questions = db.Questions.ToList();
-            ViewBag.Answers = db.StudentAnswers.ToList();
-            ViewData["ExamId"] = id;
-            
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(string AccessCode)
+        {
+            string username = (string)Session["Username"];
+            if (db.ExamSessions.Where(x => x.AccessCode == AccessCode).FirstOrDefault() != null)
+            {
+                string id = db.ExamSessions.Where(x => x.AccessCode == AccessCode).FirstOrDefault().ExamId;
+
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                ExamSession examSession = await db.ExamSessions.FindAsync(id);
+                if (examSession == null)
+                {
+                    return HttpNotFound();
+                }
+
+                // we get the paperquestions for this exam in a list
+                List<PaperQuestion> paper = db.PaperQuestions.Where(x => x.ExamId == id).ToList();
+
+                //now we generate a list of QuestionId found in the list above
+                IEnumerable<int> specificQuestions = from p in paper select p.QuestionId;
+
+                //and we select those Questions that match the QuestionIds
+                List<Question> questionList = new List<Question>(from q in db.Questions where specificQuestions.Contains(q.QuestionId) select q);
+
+                //now we get the multiple choices we need for this exam
+                List<MultipleChoice> multiChoiceList = new List<MultipleChoice>(from m in db.MultipleChoices where specificQuestions.Contains(m.QuestionId) select m);
+
+                ViewBag.MultiChoices = multiChoiceList;
+                ViewBag.Questions = questionList;
+                ViewBag.PaperQuestions = paper;
+
+                //ViewBag.PaperQuestions = db.PaperQuestions.Where(x => x.ExamId == id).ToList();
+                //ViewBag.Questions = db.Questions.ToList();
+
+                //we get the StudentAnswers that concern us only
+                string[] idSplit = id.Split('-');
+                username = username + "-" + idSplit[0];
+                IEnumerable<string> specificPaperQuestions = from p in paper select p.PaperQuestionId;
+                List<StudentAnswer> answerList = new List<StudentAnswer>(from q in db.StudentAnswers where specificPaperQuestions.Contains(q.PaperQuestionId) select q);
+                List<StudentAnswer> specificAnswerList = answerList.Where(x => x.EnrollmentId == username).ToList();
+                ViewBag.Answers = specificAnswerList;
+                ViewData["ExamId"] = id;
+
+                //now we get the subject
+                string subjectId = (from q in questionList select q.RelatedSubject.SubjectName).FirstOrDefault();
+                ViewBag.Subject = subjectId;
+
+                return View();
+            }
+            else
+            {
+                ViewData["Error"] = "This code is not available";
+                return View();
+            }
         }
         
         [HttpPost]
         //[ValidateAntiForgeryToken]
-        public async Task<ActionResult> Index([Bind(Include = "AnswerId,EnrollmentId,PaperQuestionId,Answer,CommittedByStudent")] StudentAnswer studentAnswer)
+        public async Task<ActionResult> Answer([Bind(Include = "AnswerId,EnrollmentId,PaperQuestionId,Answer,CommittedByStudent")] StudentAnswer studentAnswer)
         {
+            string username = (string)Session["Username"];
+
+            string exam = (from d in db.PaperQuestions where d.PaperQuestionId == studentAnswer.PaperQuestionId select d.ExamId).SingleOrDefault();
+
+            // we get the paperQuestions for this exam in a list
+            List<PaperQuestion> paper = db.PaperQuestions.Where(x => x.ExamId == exam).ToList();
+
+            //now we generate a list of QuestionId found in the list above
+            IEnumerable<int> specificQuestions = from p in paper select p.QuestionId;
+
+            //and we select those Questions that match the QuestionIds
+            List<Question> questionList = new List<Question>(from q in db.Questions where specificQuestions.Contains(q.QuestionId) select q);
+
+            //now we get the multiple choices we need for this exam
+            List<MultipleChoice> multiChoiceList = new List<MultipleChoice>(from m in db.MultipleChoices where specificQuestions.Contains(m.QuestionId) select m);
+
+            ViewBag.MultiChoices = multiChoiceList;
+            ViewBag.Questions = questionList;
+            ViewBag.PaperQuestions = paper;
+
+            //we get the StudentAnswers that concern us only
+            string[] idSplit = exam.Split('-');
+            username = username + "-" + idSplit[0];
+            IEnumerable<string> specificPaperQuestions = from p in paper select p.PaperQuestionId;
+            List<StudentAnswer> answerList = new List<StudentAnswer>(from q in db.StudentAnswers where specificPaperQuestions.Contains(q.PaperQuestionId) select q);
+            List<StudentAnswer> specificAnswerList = answerList.Where(x => x.EnrollmentId == username).ToList();
+            ViewBag.Answers = specificAnswerList;
+            ViewData["ExamId"] = exam;
+
+
+            //now we get the subject
+            string subjectId = (from q in questionList select q.SubjectId).FirstOrDefault();
+            ViewBag.Subject = subjectId;
+
             if (ModelState.IsValid)
             {
-                StudentAnswer newStudentAnswer = db.StudentAnswers.Where(x => x.EnrollmentId == studentAnswer.EnrollmentId).Where(x => x.PaperQuestionId == studentAnswer.PaperQuestionId).FirstOrDefault();
-                if (newStudentAnswer != null)
+                //check that student is really enrolled
+                if (db.Enrollments.Find(username) != null)
                 {
-                    studentAnswer.AnswerId = newStudentAnswer.AnswerId;
-                }
-                db.StudentAnswers.AddOrUpdate(studentAnswer);
+                    StudentAnswer newStudentAnswer = db.StudentAnswers
+                        .Where(x => x.EnrollmentId == studentAnswer.EnrollmentId)
+                        .Where(x => x.PaperQuestionId == studentAnswer.PaperQuestionId).FirstOrDefault();
+                    if (newStudentAnswer != null)
+                    {
+                        studentAnswer.AnswerId = newStudentAnswer.AnswerId;
+                    }
 
-                await db.SaveChangesAsync();
+                    db.StudentAnswers.AddOrUpdate(studentAnswer);
+
+                    await db.SaveChangesAsync();
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
             }
 
-            return View();
+            return View("Index");
         }
     }
 }
