b301ef56e23e195c8fa0d2f0bd3178bfcbc5b103
CHAKOTAY99 <40712736+CHAKOTAY99@users.noreply.github.com>
2019-04-17 23:48:29 +0200

    Examiner Dashboard and Login

--- Version1/Test2/Test2/Controllers/AccountController.cs
+++ Version1/Test2/Test2/Controllers/AccountController.cs
@@ -105,7 +105,7 @@ namespace Test2.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
+                    ModelState.AddModelError("", "Invalid login attempt!");
                     return View(model);
             }
         }
