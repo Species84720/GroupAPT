4a2d6e937f1aa199ac5bd3c3c751d5af87cec6f7
MarkMifsud <40730710+MarkMifsud@users.noreply.github.com>
2019-05-30 15:39:14 +0200

    removed seeding of test data from migration

--- POEMS/Test2/Migrations/201903221058244_seedMoreTestData.cs
+++ POEMS/Test2/Migrations/201903221058244_seedMoreTestData.cs
@@ -8,7 +8,7 @@ namespace Test2.Migrations
         public override void Up()
         {
 
-            Sql(@"
+           /* Sql(@"
 INSERT INTO [dbo].[UserRole] ([UserId], [RoleId]) VALUES (N'143b7e0b-dc92-46ee-913a-78a4a3db8ec8', N'1')
 
 INSERT INTO [dbo].[AllUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName], [FirstName], [Surname], [DepartmentId], [NickName]) VALUES (N'7cf12b6b-845f-4005-9d15-66ede95a3412', N'tt@mail.com', 0, N'AF4bBPebZ/Ybzoc7dxjTfCEbYOzWuJRR4EJEW3FThcjyHyb33vwiT2Ag5fajsVNImg==', N'910609db-32b5-4c95-b1d7-8be3db7ea253', NULL, 0, 0, NULL, 1, 0, N'TerTea19', N'Terry', N'Teacher', 4, N'TerTea19')
@@ -19,7 +19,7 @@ INSERT INTO [dbo].[UserRole] ([UserId], [RoleId]) VALUES (N'7cf12b6b-845f-4005-9
 SET IDENTITY_INSERT [dbo].[Teachings] ON
 INSERT INTO [dbo].[Teachings] ([ExaminerId], [SubjectId], [TeachingId]) VALUES (N'7cf12b6b-845f-4005-9d15-66ede95a3412', N'TST2019', 1)
 SET IDENTITY_INSERT [dbo].[Teachings] OFF
-");
+"); */
         }
         
         public override void Down()
